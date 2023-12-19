dbfc113710d5f1f24050e8a7f4ff2ff3a2459021
Claudia <claudiarichardxx@gmail.com>
2023-12-19 01:59:15 -0500

    Update setup.py

--- Search Engine - Backend/setup.py
+++ Search Engine - Backend/setup.py
@@ -1,9 +1,9 @@
 from transformers import AutoTokenizer, AutoModel
 import torch
 
-model = 'sentence-transformers/all-MiniLM-L6-v2'
-mini_lm_model = AutoModel.from_pretrained(model, torch_dtype= torch.float32)
-mini_lm_tokenizer = AutoTokenizer.from_pretrained(model)
+model = 'sentence-transformers/all_mpnet_base_v2'
+all_mpnet_base_model = AutoModel.from_pretrained(model, torch_dtype= torch.float32)
+all_mpnet_base_tokenizer = AutoTokenizer.from_pretrained(model)
 
-mini_lm_model.save_pretrained("Models/")
-mini_lm_tokenizer.save_pretrained("Models/Tokenizer/")
+all_mpnet_base_model.save_pretrained("Models/")
+all_mpnet_base_tokenizer.save_pretrained("Models/Tokenizer/")
